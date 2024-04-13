5ba4cc1b5132f9387672d8eeab0fff3334af50ef
marksikaundi <emmanuelmark117@gmail.com>
2024-04-14 01:40:27 +0200

    progressing with chatlist

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,8 +1,20 @@
+import  { useState } from 'react' 
 import "./chaList.css"
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false)
+
+
   return (
     <div className="chatList">
-      Chatlist
+      <div className="search">
+        <div className="searchBar">
+          <img className="searchImage" src="./search.png" alt="" />
+          <input className="search__input" type="text" placeholder="Search" />
+
+        </div>
+        <img className="add" src={addMode ? "./minus.png" : './plus.png'} alt=""
+        onClick={() => setAddMode((prev)=>!prev)} />
+      </div>
     </div>
   )
 }
