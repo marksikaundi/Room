44dae6fa490189175e624f67f3beadee301a3d5c
marksikaundi <emmanuelmark117@gmail.com>
2024-04-14 01:41:56 +0200

    formated the base

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,8 +1,7 @@
-import  { useState } from 'react' 
-import "./chaList.css"
+import { useState } from "react";
+import "./chaList.css";
 const ChatList = () => {
-  const [addMode, setAddMode] = useState(false)
-
+  const [addMode, setAddMode] = useState(false);
 
   return (
     <div className="chatList">
@@ -10,13 +9,16 @@ const ChatList = () => {
         <div className="searchBar">
           <img className="searchImage" src="./search.png" alt="" />
           <input className="search__input" type="text" placeholder="Search" />
-
         </div>
-        <img className="add" src={addMode ? "./minus.png" : './plus.png'} alt=""
-        onClick={() => setAddMode((prev)=>!prev)} />
+        <img
+          className="add"
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          onClick={() => setAddMode((prev) => !prev)}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatList
+export default ChatList;
